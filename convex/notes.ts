12d8e113963dc6288dfbadddb8d3e3a22f2a4410
Kartik Maheshwari <maheshwarikartik1902@gmail.com>
2026-07-15 03:39:25 +0530

    feat: view notes APIs

--- convex/notes.ts
+++ convex/notes.ts
@@ -22,10 +22,29 @@ export const getNotes = query({
     async handler(ctx){
         const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
         if(!userId){
-            throw new ConvexError("Unauthorized");
+            return null;
         };
         const notes = await ctx.db.query("notes").withIndex("by_token_identifier", (q) => q.eq("tokenIdentifier", userId))
         .collect();
         return notes;
+    }
+})
+
+
+export const getNote = query({
+    args: {
+        noteId: v.id("notes"),
+    },
+    async handler(ctx, args){
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+
+        if(!userId){
+            return null;
+        };
+        const note = await ctx.db.get("notes", args.noteId);
+        if(note?.tokenIdentifier !== userId){
+           return null;
+        }
+        return note;
     }
 })
