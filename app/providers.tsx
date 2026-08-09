c846c221f3d867bb64081f30dad87f3a6645aa23
Kartik Maheshwari <maheshwarikartik1902@gmail.com>
2026-08-10 01:51:08 +0530

    fix(deploy): proxyUrl setup for dev and prod

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,7 +8,7 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as strin
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} proxyUrl="/__clerk">
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} proxyUrl={process.env.NEXT_PUBLIC_CLERK_PROXY_URL}>
             <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
                 {children}
             </ConvexProviderWithClerk>
