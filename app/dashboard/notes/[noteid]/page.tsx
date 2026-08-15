84649a69e9fb825c096cb6bebf9b542c3b0feaae
Kartik Maheshwari <maheshwarikartik1902@gmail.com>
2026-08-16 00:45:50 +0530

    fix(notes): Api parameter reolved

--- app/dashboard/notes/[noteid]/page.tsx
+++ app/dashboard/notes/[noteid]/page.tsx
@@ -6,18 +6,23 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
+import React from "react";
 
-export default function NotesPage() {
-    const params = useParams();
-    console.log("params:", params);
-    const { noteId } = useParams<{ noteId: Id<"notes"> }>();
-    console.log(noteId);
-    const note = useQuery(api.notes.getNote, { noteId });
+export default function NotesPage({ params }: { params: Promise<{ noteId: Id<'notes'> }> }) {
+    const { noteId } = React.use(params);
+    const note = useQuery(
+        api.notes.getNote,
+        noteId
+            ? {
+                noteId: noteId as Id<"notes">,
+            }
+            : "skip"
+    );
     return (
 
         <div className="flex justify-between items-center my-4 relative">
             <div className="absolute -top-5 -right-1">
-                <DeleteNoteButton noteId={noteId}/>
+                <DeleteNoteButton noteId={noteId} />
             </div>
 
             <h1 className="text-xl mx-2 mt-6 whitespace-pre-line">
